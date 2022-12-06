8be917121e7723d11e957d761b421ac5ca7fb8da
JanKrykorka <jankrykorka4@gmail.com>
2022-12-06 17:47:58 +0100

    colt 45

---
+++ Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace battleRoyal
+{
+    class Enemy
+    {
+        private int health;
+        private int maxHealth;
+        private int attackPower;
+        public Enemy(int health, int attack) 
+        {
+            this.health = health;
+            this.maxHealth = health;
+            this.attackPower = attack;
+        }
+        public bool HeLivin { get { return (health > 0); } }
+
+        public string HealthBar() 
+        {
+            string s = "[";
+            int i = 20;
+            double space = Math.Round(((double)health / maxHealth) * i);
+            if ((space == 0) && (HeLivin))
+                space = 1;
+            for (int j = 0; j < space; j++)
+                s += "#";
+            s = s.PadRight(i + 1);
+            s += "]";
+            return s;
+        }
+
+
+        public Enemy PickOpponent(Enemy[] opponents)
+        {
+            int i = Random.Shared.Next(opponents.Length);
+            return opponents[i];
+        }
+
+        public int ReceiveDamage(int incomingDamage)
+        {
+            if (incomingDamage > health)
+                health = 0;
+            else
+                health -= incomingDamage;
+            return health;
+        }
+
+        public int Attack( Enemy target)
+        {
+            Console.WriteLine(this + "hits" + target + "for" + attackPower);
+            return target.ReceiveDamage(attackPower);
+        }
+
+
+        
+
+
+     
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using battleRoyal;
+
+namespace battleRoyal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Enemy JoeNutz = new Enemy(100, 20);
+            Enemy MikeHawkHertz = new Enemy(120, 10);
+            // Fighter BigusNIgus = new Fighter("Bigus Gayus Nigga barlsus", 100) Attack*0.25)
+            // Knight MikeOxlong = new Knight("Mike Oxlong", 100,) RecieveDamage*0.10)
+            // Sorcerer BenDover = new Sorcerer("Ben Dover", 100, 100(Mana))
+            // Beast
+            Enemy[] combatants = new Enemy[] { JoeNutz, MikeHawkHertz };
+            int combAlive = combatants.Length;
+            int round = 0;
+            while (combAlive > 1)
+            {
+                Console.WriteLine("\\--//" + round++ + "\\--//");
+                foreach (var c in combatants)
+                {
+                    var x = c.PickOpponent(combatants);
+                    c.Attack(x);
+                }
+                combAlive = 0;
+                foreach (var c in combatants)
+                {
+                    if (c.HeLivin) combAlive++;
+                    Console.WriteLine(c);
+                }
+                Console.WriteLine(" Joe Nutz livin: {0}", JoeNutz.HeLivin);
+                Console.WriteLine("Joe Nutz's Health bar be like: {0}", JoeNutz.HealthBar());
+                Console.WriteLine("Mike Hawk Hertz livin: {0}", MikeHawkHertz.HeLivin);
+                Console.WriteLine("Mike Hawk Hertz's Health bar be like: {0}", MikeHawkHertz.HealthBar());
+            }
+        }
+    }
+}
